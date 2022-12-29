d50f4d7a318da99d26c896daa09b9c24192f6453
Sinaver Idris <sinavercrimea@gmail.com>
2022-12-29 21:11:23 +0400

    basic intergration with MUI

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,28 @@ import { useEffect, useState } from 'react';
 import deploy from './deploy';
 import Escrow from './Escrow';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/system/Container';
+
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+
+import { createTheme } from '@mui/material/styles';
+import blue from '@mui/material/colors/blue';
+import yellow from '@mui/material/colors/yellow';
+
+import ThemeProvider from '@mui/material/styles/ThemeProvider'
+
+const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: yellow
+  },
+});
+
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 export async function approve(escrowContract, signer) {
@@ -55,45 +77,68 @@ function App() {
 
   return (
     <>
-      <div className="contract">
-        <h1> New Contract </h1>
-        <label>
-          Arbiter Address
-          <input type="text" id="arbiter" />
-        </label>
+      <ThemeProvider theme={theme}>
+        <Container sx={{ flexGrow: 5 }}>
 
-        <label>
-          Beneficiary Address
-          <input type="text" id="beneficiary" />
-        </label>
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="absolute" color='default'>
+              <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  Escrow App
+                </Typography>
+                <Button variant="outlined" endIcon={<AccountBalanceWalletIcon />}>
+                  Connect
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </Box>
 
-        <label>
-          Deposit Amount (in Eth)
-          <input type="text" id="eth" />
-        </label>
 
-        <div
-          className="button"
-          id="deploy"
-          onClick={(e) => {
-            e.preventDefault();
+          <Box position="relative">
+            <div className="contract">
+              <h1> New Contract </h1>
+              <label>
+                Arbiter Address
+                <input type="text" id="arbiter" />
+              </label>
 
-            newContract();
-          }}
-        >
-          Deploy
-        </div>
-      </div>
+              <label>
+                Beneficiary Address
+                <input type="text" id="beneficiary" />
+              </label>
 
-      <div className="existing-contracts">
-        <h1> Existing Contracts </h1>
+              <label>
+                Deposit Amount (in Eth)
+                <input type="text" id="eth" />
+              </label>
 
-        <div id="container">
-          {escrows.map((escrow) => {
-            return <Escrow key={escrow.address} {...escrow} />;
-          })}
-        </div>
-      </div>
+              <div
+                className="button"
+                id="deploy"
+                onClick={(e) => {
+                  e.preventDefault();
+
+                  newContract();
+                }}
+              >
+                Deploy
+              </div>
+            </div>
+          </Box>
+          <Box position="relative">
+            <div className="existing-contracts">
+              <h1> Existing Contracts </h1>
+
+              <div id="container">
+                {escrows.map((escrow) => {
+                  return <Escrow key={escrow.address} {...escrow} />;
+                })}
+              </div>
+            </div>
+          </Box>
+        </Container>
+      </ThemeProvider>
+
     </>
   );
 }
